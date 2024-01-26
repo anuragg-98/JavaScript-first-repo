9180a29047eb3906862bfebc316ebdd33beafb7e
anuragg-98 <123328075+anuragg-98@users.noreply.github.com>
2024-01-26 20:46:08 +0000

    operations done

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -26,3 +26,53 @@ let someNumber = undefined
 let stringNumber = String(someNumber)
 // console.log(stringNumber);
 // console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);    // 2**3 => 2^3
+// console.log(2/3);
+// console.log(2%3);     // modulous operator
+
+let str1 = "Hello"
+let str2 = " Anurag"
+
+let str3 = str1 + str2  // sums up 2 strings.
+// console.log(str3);  
+
+// console.log("1" + 2);            // 12
+// console.log(1 + "2");            // 12
+// 1. console.log("1" + 2 + 2);        // 122
+// 2. console.log(1 + 2 + "2");        // 32
+
+    /*
+    --> The addition operation is performed from left to right. 
+    --> First, "1" + 2 results in the string "12".
+    --> Then, the result "12" is concatenated with the number 2,
+    --> resulting in the string "122".
+    */
+
+    /* 
+    --> So, In 2nd, 1 + 2 results in 3. Then, the string concatenation operation is 
+    --> performed with the result (3) and the string "2", resulting in string "32". 
+    */
+
+
+// console.log( (3 + 4) * 5 % 3);   // useless format
+
+// console.log(+true);  // 1
+// console.log(+"");    // 0
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
