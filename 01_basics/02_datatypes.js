771b8b8306ea5a0857703b89be9e29d15f6ad153
anuragg-98 <123328075+anuragg-98@users.noreply.github.com>
2024-01-26 09:40:51 +0000

    datatypes done

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,25 @@
+"use strict"; // treat all JS code as newer version
+
+let name = "hitesh"
+let age = 18
+let isLoggedIn = false
+let state;
+
+// number => range (2 to power 53). It can be an integer or a floating-point number.
+// bigint
+// string => " "
+// boolean => true/false
+// null => standalone value (Represents the intentional absence of any object value.)
+// undefined => Represents a variable that has been declared but has not been assigned a value.
+
+// symbol => A unique and immutable primitive value. Symbols are often used as unique identifiers for object properties.
+
+ const mySymbol = Symbol("description");
+
+// object => Represents a collection of key-value pairs. Each key is a string, and values can be of any data type.
+
+
+console.log(typeof undefined); // undefined
+console.log(typeof null); // object
+
+// 'typeof' is a keyword for getting the datatype.
